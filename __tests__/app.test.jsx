95ed2b242d664abcf9c0c4653e052086f42764f1
Erik Graciosa <70562690+ErikGraciosa@users.noreply.github.com>
2021-03-10 07:57:53 -0800

    Added auth tests (#31)
    * backend creating paymentIntent
    
    * scaffold file structure
    
    * added transaction service and middlewear
    
    * broken-transactions
    
    * added auth tests
    
    * removed console logs
    
    * updated yml

--- __tests__/app.test.jsx
+++ __tests__/app.test.jsx
@@ -2,14 +2,87 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const TenantService = require('../lib/services/TenantService');
 
 describe('shorten-be routes', () => {
-    beforeEach(() => {
-        return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
-    });
+  beforeEach(async() => {
+    await pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+    return pool.query(fs.readFileSync('./sql/seedDatabase.sql', 'utf-8'));
+  });
 
-    it('passes', () => {
-        const response = 'true';
-        expect(response).toEqual('true');
+  afterAll(pool.end);
+
+  it('allows a user to signup via POST', async() => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ 
+        email: 'test@test.com', 
+        password: 'password',
+        name: 'Jon Arbys',
+        ownerId: '1',
+        monthlyCost: 400 })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          email: 'test@test.com',
+          name: 'Jon Arbys',
+          ownerId: '1',
+          monthlyCost: 400
+        });
+      });
+  });
+    
+  it('allows a user to login via POST', async() => {
+    const user = await TenantService.create({
+      email: 'test@test.com',
+      password: 'password',
+      name: 'Jon Arbys',
+      ownerId: '1',
+      monthlyCost: 400
     });
+    
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+    
+    expect(res.body).toEqual({
+      id: user.id,
+      email: 'test@test.com',
+      name: 'Jon Arbys',
+      ownerId: '1',
+      monthlyCost: 400
+    });
+  });
+    
+  it('verifies a user is logged in', async() => {
+    const agent = request.agent(app);
+    const user = await TenantService.create({
+      email: 'test@test.com',
+      password: 'password',
+      name: 'Jon Arbys',
+      ownerId: '1',
+      monthlyCost: 400
+    });
+    
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+    
+    const res = await agent
+      .get('/api/v1/auth/verify');
+    
+    expect(res.body).toEqual({
+      id: user.id,
+      email: 'test@test.com',
+      name: 'Jon Arbys',
+      ownerId: '1',
+      monthlyCost: 400
+    });
+  });
 });
--- sql/seedDatabase.sql
+++ sql/seedDatabase.sql
@@ -3,8 +3,8 @@ INSERT INTO owners (connected_acct_id, property_address) VALUES
 ('acct_1IQL66Ie12345678', '807 Bit Apt 1, Incline Village, NV 89451'),
 ('acct_1IQL66Ie91011121', '711 Code Ave, Portland, OR 97214');
 
-INSERT INTO tenants (name, email, password_hash, property_address, owner_id, monthly_cost) VALUES 
-('tenant-A', 'tenant-A@gmail.com', 'hashed1234', '8 Byte Drive Apt C, Loma Vista, CA 90210', 1, 500),
-('tenant-B', 'tenant-B@gmail.com', 'hashedpswd', '8 Byte Drive Apt C, Loma Vista, CA 90210', 1, 400),
-('tenant-C', 'tenant-C@gmail.com', 'hashedhullo', '8 Byte Drive Apt C, Loma Vista, CA 90210', 1, 500),
-('tenant-D', 'tenant-D@gmail.com', 'hashednice', '807 Bit Apt 1, Incline Village, NV 89451', 2, 1200);
+INSERT INTO tenants (name, email, password_hash, owner_id, monthly_cost) VALUES 
+('tenant-A', 'tenant-A@gmail.com', 'hashed1234', 1, 500),
+('tenant-B', 'tenant-B@gmail.com', 'hashedpswd', 1, 400),
+('tenant-C', 'tenant-C@gmail.com', 'hashedhullo', 1, 500),
+('tenant-D', 'tenant-D@gmail.com', 'hashednice', 2, 1200);
